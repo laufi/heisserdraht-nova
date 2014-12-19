0fae4e80185767c4f80b54fd608c600da81c5c1f
laufi <laufi_github@outlook.com>
2014-12-19 23:36:04 +0100

    Tester für Anonymous hinzugefügt

--- src/test/java/io/github/laufi/heisserdraht/util/HighscoreEintragTest.java
+++ src/test/java/io/github/laufi/heisserdraht/util/HighscoreEintragTest.java
@@ -18,4 +18,17 @@ public class HighscoreEintragTest{
 		assertTrue(eintrag.getName().equals("test"));
 		assertEquals(eintrag.getScore(), 101);
 	}
+	@Test
+	public void testPOJOConstructorAnonymous (){
+		HighscoreEintrag eintrag = new HighscoreEintrag(101);
+		assertTrue(eintrag.getName().equals("Anonymous"));
+		assertEquals(eintrag.getScore(), 101);
+	}
+	@Test
+	public void testPOJOSetsAnonymous (){
+		HighscoreEintrag eintrag = new HighscoreEintrag();
+		eintrag.setScore(101);
+		assertTrue(eintrag.getName().equals("Anonymous"));
+		assertEquals(eintrag.getScore(), 101);
+	}
 }
