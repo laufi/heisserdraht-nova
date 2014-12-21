710e73a15b8c176c8e1a7f64080d9dea11df1117
laufi <laufi_github@outlook.com>
2014-12-21 15:46:27 +0100

    Dependency zu jfiglet eingefügt. Dependency auch in JAR eingefügt

---
+++ src/main/java/io/github/laufi/heisserdraht/Main.java
@@ -0,0 +1,10 @@
+package io.github.laufi.heisserdraht;
+
+/**
+ * Created by laurenz on 20.12.14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
