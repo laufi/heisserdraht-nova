6659514b289ccb3c4703506c60edf224492490dd
laufi <laufi_github@outlook.com>
2014-12-22 18:33:38 +0100

    Ein- und Ausgabe implementiert

--- src/main/java/io/github/laufi/heisserdraht/util/Highscore.java
+++ src/main/java/io/github/laufi/heisserdraht/util/Highscore.java
@@ -1,5 +1,14 @@
 package io.github.laufi.heisserdraht.util;
 
-public class Highscore {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Highscore {
+    private List<HighscoreEintrag> score = new ArrayList<HighscoreEintrag>();
+    public void addScore(HighscoreEintrag score){
+        this.score.add(score);
+    }
+    public HighscoreEintrag getScore (int nummer){
+        return this.score.get(nummer);
+    }
 }
