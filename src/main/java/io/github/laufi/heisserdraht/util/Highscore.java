5d44dda9e13bc6b7ce998b04f56554d4203d2861
laufi <laufi_github@outlook.com>
2014-12-22 19:09:00 +0100

    Comparator implementiert

--- src/main/java/io/github/laufi/heisserdraht/util/Highscore.java
+++ src/main/java/io/github/laufi/heisserdraht/util/Highscore.java
@@ -1,14 +1,19 @@
 package io.github.laufi.heisserdraht.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class Highscore {
+public class Highscore{
     private List<HighscoreEintrag> score = new ArrayList<HighscoreEintrag>();
     public void addScore(HighscoreEintrag score){
         this.score.add(score);
     }
     public HighscoreEintrag getScore (int nummer){
         return this.score.get(nummer);
+    }
+    public void sortScore(Comparator comparator){
+        Collections.sort(this.score, comparator);
     }
 }
